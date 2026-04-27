3709aa4cd10fc61d292f49a5cb603e8237c953eb
Damián Martínez <pmg0063@alu.medac.es>
2026-04-27 10:04:16 +0200

    comentar  lo que hace la función

--- lib/services/image_saver_service.dart
+++ lib/services/image_saver_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
-
+//esta funcion guarda las imagenes en local y te devuelve el path para dejarlo en la base de datos y que se vea la imagen
 Future<List<String>> saveImages(List<File> images) async {
   final directory = await getApplicationDocumentsDirectory();
 
