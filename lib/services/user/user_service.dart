83945b7b9cab31d9cc5c0aff71a497c0824e4ce1
Diab Miro <dmm0041@alu.medac.es>
2026-06-01 11:18:09 +0200

    Separados administradores en colección propia y simplificadas reglas Firestore

--- lib/services/user/user_service.dart
+++ lib/services/user/user_service.dart
@@ -8,21 +8,37 @@ class UserService {
   final FirebaseFirestore _firestore;
 
   Future<UserModel> getUser(String uid) async {
-    final doc = await _firestore.collection('users').doc(uid).get();
-    if (!doc.exists || doc.data() == null) {
+    final adminDoc = await _firestore.collection('admin').doc(uid).get();
+
+    if (adminDoc.exists && adminDoc.data() != null) {
+      final data = Map<String, dynamic>.from(adminDoc.data()!);
+      data['role'] = 'admin';
+      return UserModel.fromMap(data, uid);
+    }
+
+    final userDoc = await _firestore.collection('users').doc(uid).get();
+
+    if (!userDoc.exists || userDoc.data() == null) {
       throw Exception('Usuario no encontrado: $uid');
     }
-    return UserModel.fromMap(doc.data()!, uid);
+
+    final data = Map<String, dynamic>.from(userDoc.data()!);
+    data['role'] = data['role'] ?? 'customer';
+
+    return UserModel.fromMap(data, uid);
   }
 
   Future<void> updateProfile({
     required String uid,
     required String name,
     required String surname,
-  }) {
-    return _firestore.collection('users').doc(uid).update({
+  }) async {
+    final profileRef = await _profileDocumentRef(uid);
+
+    return profileRef.update({
       'name': name,
       'surname': surname,
+      'updated_at': FieldValue.serverTimestamp(),
     });
   }
 
@@ -36,6 +52,20 @@ class UserService {
       'nautical_license.type': type,
       'nautical_license.document_url': documentUrl,
       'nautical_license.status': status,
+      'updated_at': FieldValue.serverTimestamp(),
     });
+  }
+
+  Future<DocumentReference<Map<String, dynamic>>> _profileDocumentRef(
+    String uid,
+  ) async {
+    final adminRef = _firestore.collection('admin').doc(uid);
+    final adminDoc = await adminRef.get();
+
+    if (adminDoc.exists) {
+      return adminRef;
+    }
+
+    return _firestore.collection('users').doc(uid);
   }
 }
